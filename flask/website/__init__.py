3b6e5193abafdfbde4535a3ae6013fcced452db5
Bspier <Bspier95@gmail.com>
2023-04-24 22:07:36 -0700

    4/24 changes

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -7,16 +7,12 @@ from flask_login import LoginManager
 def create_app():
     app= Flask(__name__)
     app.config["SECRET_KEY"]  = "SECRET"
-
-    '''@app.route("/home")
-    def home():
-        return "<h1>HOME</h1>"'''
     
     '''. for relative import'''
     from .views import views
-    from .auth import auth 
+
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+
 
     return app
--- flask/website/views.py
+++ flask/website/views.py
@@ -6,3 +6,47 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 def home():
     return render_template("home.html", var="value")
+
+@views.route("/help")
+def help():
+    return render_template("help.html", var="value")
+
+@views.route("/about")
+def about():
+    return render_template("about.html", var="value")
+
+
+'''player stats route'''
+@views.route("/player-stats")
+def player_stats():
+    return render_template("player_stats.html", var="value")
+
+@views.route("/player-name")
+def player_name():
+    return render_template("player_name.html", var="value")
+
+@views.route("/player-random")
+def player_random():
+    return render_template("player_random.html", var="value")
+
+@views.route("/player-compare")
+def player_compare():
+    return render_template("player_comp.html", var="value")
+
+
+'''Team stats routes'''
+@views.route("/team-stats")
+def team_stats():
+    return render_template("team_stats.html", var="value")
+
+@views.route("/team-name")
+def team_name():
+    return render_template("team_name.html", var="value")
+
+@views.route("/team-random")
+def team_random():
+    return render_template("team_random.html", var="value")
+
+@views.route("/team-compare")
+def team_compare():
+    return render_template("team_comp.html", var="value")
